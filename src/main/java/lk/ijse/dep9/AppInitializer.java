8a2b4f88e63e7a22da343cbea347d92c24f55a86
pasanweerawardana <pasanm16gmail.com>
2022-09-04 13:52:53 +0530

    commit 3

--- src/main/java/lk/ijse/dep9/AppInitializer.java
+++ src/main/java/lk/ijse/dep9/AppInitializer.java
@@ -1,7 +1,11 @@
 package lk.ijse.dep9;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class AppInitializer extends Application {
 
@@ -10,7 +14,11 @@ public class AppInitializer extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) {
-
+    public void start(Stage primaryStage) throws IOException {
+        primaryStage.setScene(new Scene(FXMLLoader.load(this.getClass().getResource("/view/LoginForm.fxml"))));
+        primaryStage.setTitle("Open Source Medical Clinic");
+        primaryStage.setResizable(false);
+        primaryStage.show();
+        primaryStage.centerOnScreen();
     }
 }
